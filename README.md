1efd5c4474dccee3851e9bbefb97c01292091f26
Po1onium <zhanggianluca2@gmail.com>
2023-01-10 22:02:23 -0500

    Cargo Manifests

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Level 7 Project: Cargo Manifests
 ## Gianluca Zhang Web Development EVEN Period 7/8
+
+#340068 Persian Indigo
+#FF6978 Ultra Red
+#FFFCF9 Baby Powder
+#6D435A Eggplant
---
+++ script.js
@@ -0,0 +1,24 @@
+function initialize() {
+    const ITEM_LIST = [
+        {"name": "Gloves", "img":"🧤", "size":1}, 
+        {"name": "Sunglasses", "img":"🕶", "size":1},
+        {"name": "Sneakers", "img":"👟", "size":2},
+        {"name": "Camera", "img":"📷", "size":3},
+        {"name": "Radio", "img":"📻", "size":4},
+        {"name": "Computer Keyboard", "img":"⌨️", "size":4},
+        {"name": "Laptop", "img":"💻", "size":5},
+        {"name": "Desktop", "img":"🖥", "size":6},
+        {"name": "Printer", "img":"🖨", "size":7}, 
+        {"name": "Television", "img":"📺", "size":8}, 
+        {"name": "Bathtub", "img":"🛁", "size":10},
+        {"name": "Bed", "img":"🛏", "size":10},
+        {"name": "Telescope", "img":"🔭", "size":7},
+        {"name": "Barrel", "img":"🛢", "size":9},
+        {"name": "Couch", "img":"🛋", "size":9},
+        {"name": "Bicycle", "img":"🚲", "size":9},
+        {"name": "Scooter", "img":"🛴", "size":8},
+        {"name": "Baseball", "img":"⚾️", "size":2},
+        {"name": "Volleyball", "img":"🏐", "size":3},
+        {"name": "Socks", "img":"🧦", "size":1}
+    ]
+}
